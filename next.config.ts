819fda55ff376565ffc59481d2a8148caff9f312
Jihyun An <hwiyu25@gmail.com>
2025-02-16 20:58:57 +0900

    timeout 3분으로 늘림

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    proxyTimeout: 1000 * 60 * 3, // 추후 lambda 호출 api만 처리
+  },
 };
 
 export default nextConfig;
