b7aa5934293da37832a82049437972446281c910
Jihyun An <hwiyu25@gmail.com>
2025-02-18 22:28:00 +0900

    header - Notion CV 텍스트 추가

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -6,9 +6,12 @@ export default function Header() {
   return (
     <header className="t-0 fixed left-0 flex h-11 w-full items-center border-b border-b-gray-200 bg-white pr-6 pl-6">
       <Link href={ROUTES.HOME}>
-        <figure>
-          <Image src="/notion-cv.svg" alt="Notion CV logo" width={32.94} height={20} priority />
-        </figure>
+        <div className="flex items-center gap-2.5">
+          <figure>
+            <Image src="/notion-cv.svg" alt="Notion CV logo" width={32.94} height={20} priority />
+          </figure>
+          <h1 className="text-md font-medium">Notion CV</h1>
+        </div>
       </Link>
     </header>
   );
