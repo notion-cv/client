64896c69beb8093befa4c45b5942db6f29d7721d
Jihyun An <hwiyu25@gmail.com>
2025-02-19 16:13:30 +0900

    ga event 확인

--- src/components/features/preview/PageGuard.tsx
+++ src/components/features/preview/PageGuard.tsx
@@ -17,9 +17,10 @@ export default function PageGuard() {
   useEffect(() => {
     if (!fileId) {
       alert('업로드 된 파일이 확인되지 않습니다.\n다시 시도해 주세요.');
+      sendGAEvent('denied_preview_page', { description: 'noFileId' });
       router.replace(ROUTES.HOME);
     }
-    sendGAEvent('conversion');
+    sendGAEvent('visit_preview_page');
   }, []);
 
   return null;
