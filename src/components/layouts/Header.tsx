dbdec771846f63a3db9c4bb192330e79f0ef8fff
Jihyun An <hwiyu25@gmail.com>
2025-03-30 15:27:23 +0900

    notion logo 제거

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,5 +1,4 @@
 import { NOTION_CV_LANDING_URL } from '@/constants/link';
-import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Header() {
@@ -7,9 +6,6 @@ export default function Header() {
     <header className="t-0 fixed left-0 flex h-11 w-full items-center border-b border-b-gray-200 bg-white pr-6 pl-6">
       <Link href={NOTION_CV_LANDING_URL}>
         <div className="flex items-center gap-2.5">
-          <figure>
-            <Image src="/notion-cv.svg" alt="Notion CV logo" width={32.94} height={20} priority />
-          </figure>
           <h1 className="text-md font-medium">Notion CV</h1>
         </div>
       </Link>
